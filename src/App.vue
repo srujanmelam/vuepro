c242617ae6ec21665cfebf451c4543d83ddb0749
srujanmelam <srujanmelam@gmail.com>
2021-04-30 16:26:47 +0530

    sample

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+   <h1>{{ title }}</h1>
+   <p>{{greeting()}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title:'new file!!'
+      
+    }
+  },
+  methods: {
+    greeting:function(){
+      return 'heey cowboy!!';
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
